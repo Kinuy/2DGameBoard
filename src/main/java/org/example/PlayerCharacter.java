87c7208207c89ab5a526a83a4cb54c8ae6ec6ce1
Tobias Weber <toby-weber@gmx.de>
2024-09-30 12:44:59 +0200

    implement tests_move_

--- src/main/java/org/example/PlayerCharacter.java
+++ src/main/java/org/example/PlayerCharacter.java
@@ -1,8 +1,35 @@
 package org.example;
 
 public class PlayerCharacter {
+
+    static int X,Y =0;
+
     public static int getX(){
 
         return 0;
+    }
+
+    public static int getY() {
+        return 0;
+    }
+    public static boolean move(String value) {
+        if(value == "W") {
+            int Y = 1;
+            return true;
+        }
+        if(value == "S"){
+            int Y = -1;
+            return true;
+        }
+        if(value == "D"){
+            int X = 1;
+            return true;
+        }
+        if(value == "A"){
+            int X = -1;
+            return true;
+        }
+        return false;
+
     }
 }
--- src/test/java/PlayerCharacterTest.java
+++ src/test/java/PlayerCharacterTest.java
@@ -1,4 +1,5 @@
 import org.example.PlayerCharacter;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,4 +14,31 @@ public class PlayerCharacterTest {
         //THEN
         assertEquals(0,actual);
     }
+    @Test
+    public void getYTest_whenValue0_thenReturnPositionYIsMiddle() {
+        //GIVEN
+        //WHEN
+        int actual = PlayerCharacter.getY();
+        //THEN
+        assertEquals(0,actual);
+    }
+    @Test
+    @DisplayName("Up")
+    public void moveTest_givenStringValue_W_thenSetYto1andReturnTrue(){
+        //GIVEN
+        String value = "W";
+        //WHEN
+        boolean actual = PlayerCharacter.move(value);
+        //THEN
+        assertEquals(true,actual);
+    }
+    public void moveTest_givenStringValue_W_thenSetYto1andReturnTrue(){
+        //GIVEN
+        String value = "W";
+        //WHEN
+        boolean actual = PlayerCharacter.move(value);
+        //THEN
+        assertEquals(true,actual);
+    }
 }
+
