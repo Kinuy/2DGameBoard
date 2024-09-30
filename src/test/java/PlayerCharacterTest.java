b42e59fbc731c8fcb8912821debef27a80062961
Tobias Weber <toby-weber@gmx.de>
2024-09-30 14:44:12 +0200

    implement tests_move_

--- src/test/java/PlayerCharacterTest.java
+++ src/test/java/PlayerCharacterTest.java
@@ -12,33 +12,57 @@ public class PlayerCharacterTest {
         //WHEN
         int actual = PlayerCharacter.getX();
         //THEN
-        assertEquals(0,actual);
+        assertEquals(0, actual);
     }
+
     @Test
     public void getYTest_whenValue0_thenReturnPositionYIsMiddle() {
         //GIVEN
         //WHEN
         int actual = PlayerCharacter.getY();
         //THEN
-        assertEquals(0,actual);
+        assertEquals(0, actual);
     }
+
     @Test
     @DisplayName("Up")
-    public void moveTest_givenStringValue_W_thenSetYto1andReturnTrue(){
+    public void moveTest_givenStringValue_W_thenSetYto1andReturnTrue() {
         //GIVEN
         String value = "W";
         //WHEN
         boolean actual = PlayerCharacter.move(value);
         //THEN
-        assertEquals(true,actual);
+        assertEquals(true, actual);
     }
-    public void moveTest_givenStringValue_W_thenSetYto1andReturnTrue(){
+    @Test
+    @DisplayName("Down")
+    public void moveTest_givenStringValue_S_thenSetYtoMinus1andReturnTrue() {
         //GIVEN
-        String value = "W";
+        String value = "S";
         //WHEN
         boolean actual = PlayerCharacter.move(value);
         //THEN
-        assertEquals(true,actual);
+        assertEquals(true, actual);
+    }
+    @Test
+    @DisplayName("left")
+    public void moveTest_givenStringValue_A_thenSetXtoMinus1andReturnTrue() {
+        //GIVEN
+        String value = "A";
+        //WHEN
+        boolean actual = PlayerCharacter.move(value);
+        //THEN
+        assertEquals(true, actual);
+    }
+    @Test
+    @DisplayName("Right")
+    public void moveTest_givenStringValue_D_thenSetXto1andReturnTrue() {
+        //GIVEN
+        String value = "D";
+        //WHEN
+        boolean actual = PlayerCharacter.move(value);
+        //THEN
+        assertEquals(true, actual);
     }
 }
 
